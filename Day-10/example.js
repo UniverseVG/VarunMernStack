9fcba30718b981a7ab0b587c0cc77fa55a926dfd
varun <gmavarungmavrun32524@gamil.com>
2020-11-24 18:10:24 +0530

    Added Day 10

---
+++ Day-10/example.js
@@ -0,0 +1,86 @@
+console.log("My console statement!!")
+
+
+//data types
+/* 1.Number
+2.Strings
+3.Boolean
+4.Null
+5.Undefined
+6.Objects
+7.Symbols(es6)
+Loosely Typed
+*/
+
+//syntax and declarations
+
+//var score = 10; //ESS -Ecma
+
+//let result = 50; // ES6  =Ecmascript 2015
+//const PI = 3.14; //ES6
+
+//score = 15;
+
+
+//console.log(score);
+//console.log(result);
+//console.log(PI);
+
+/*let score = 10;
+let result = '10';
+let finalscore = score + result;
+let penalty = score - result; //Mathematical
+let totalGames = '5';
+let avgScore = finalscore / totalGames;
+
+console.log(score, result);
+console.log(finalscore);
+console.log(avgScore);
+console.log(penalty);
+//typeof
+
+console.log(typeof finalscore);
+console.log(typeof penalty);
+console.log(typeof avgScore);
+
+
+let value = (finalscore += 2) + 10 / (penalty + totalGames)
+
+
+console.log(value);*/
+
+
+/*let word = "raining";
+console.log(word.toUpperCase());
+console.log("IT's " + word);
+
+
+console.log('hello and good evening'.length)
+
+console.log(typeof 1);
+console.log(typeof true);*/
+
+//console.log(isNaN('five'));
+
+/*let condition = 10 / 'hello';
+if (condition) {
+    console.Log("Very true");
+} else {
+    console.Log("very wrong!");
+}
+console.log('hello'.includes('e'));*/
+
+
+// Null Undefined
+
+let score;
+let result = null;
+let status = null;
+
+if (score) {
+    console.log("Very true");
+} else {
+    console.log("very wrong!");
+}
+console.log(typeof score);
+console.log(typeof result);
